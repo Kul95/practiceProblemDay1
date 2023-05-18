1fe051f02dc50387efb1fecaca79420d3d06757f
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 00:54:54 +0530

    Added : Default value program

--- src/com/bridglabz/PracticeProblemDay1/PracticeProblem.java
+++ src/com/bridglabz/PracticeProblemDay1/PracticeProblem.java
@@ -1,8 +1,24 @@
 package com.bridglabz.PracticeProblemDay1;
 import java.util.Scanner;
 public class PracticeProblem {
+    static int i;
+    static char ch;
+    static String str;
+    static float f;
+    static double d;
+    static long l;
+    static void  defaultValue(){
+        System.out.println(i);
+        System.out.println(ch);
+        System.out.println(str);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+    }
     public static void main(String[] args) {
+        defaultValue();
         message1();
+
     }
     public static void message1(){
         System.out.println("Enter the message:");
